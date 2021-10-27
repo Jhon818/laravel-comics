bd250de88dda260c53f2c39bf3f169ca18d1a979
giov <pintogiovanni818@gmail.com>
2021-10-27 19:35:31 +0200

    update

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,5 +4,9 @@
         <a href="{{ route('about')}}">About</a>
         <a href="{{ route('contacts')}}">Contacts</a>
     </ul>
+
+    <div id="logo">
+<img src="{{ asset('images/dc-logo.png')}}" alt="" >
+    </div>
     
 </header>
